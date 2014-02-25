1ab85cf5daab84fd33f38667a275b8fca0c304f2
Vagrant <vagrant@vagrant-debian-squeeze-64.vagrantup.com>
2014-02-25 20:30:11 +0100

    Adding first repo

---
+++ app/Core/Repos/Product/DBProductRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core\Repos\Product;
+
+use Core\Repos\AbstractRepository;
+use Product;
+
+class DbProductRepository extends AbstractRepository implements ProductRepository {
+
+    /**
+     * @var Product
+     */
+    protected $model;
+
+    /**
+     * @param Product $model
+     */
+    function __construct(Product $model)
+    {
+        $this->model = $model;
+    }
+
+}
